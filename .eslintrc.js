c3067b363c7404406af07fcaf3756fd64206f36b
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2020-02-05 22:18:33 +0000

    Pull in latest codestyles package for ECMA9/2018 for Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,4 @@
 module.exports = {
     extends: "./node_modules/@jahidulpabelislam/codestyles/node.eslint.config.js",
-    env: {
-        es2017: true,
-    },
-    parserOptions: {
-        ecmaVersion: 2017,
-    },
     root: true,
 };
