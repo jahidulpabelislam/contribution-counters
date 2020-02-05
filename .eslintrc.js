411778f87496c1eda1bef37d0e015f0adf500483
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2020-02-05 21:42:22 +0000

    WIP: Attempt to use ES2017 for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
     extends: "./node_modules/@jahidulpabelislam/codestyles/node.eslint.config.js",
+    env: {
+        es2017: true,
+    },
+    parserOptions: {
+        ecmaVersion: 2017,
+    },
     root: true,
 };
